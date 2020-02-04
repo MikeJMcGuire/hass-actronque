e1011f985a2d323c2eca8aaf2b2b8962e2be77f2
MikeJMcGuire <mike@mikejmcguire.com>
2020-02-04 11:26:15 +1100

    Initial copy of hass-actron

---
+++ hass-actronque/Controllers/DeviceController.cs
@@ -0,0 +1,158 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HMX.HASSActron.Controllers
+{
+	[Route("rest/{version:required}/block/{device:required}")]
+    public class DeviceController : Controller
+	{
+		private static int _iTimeout = 10000;
+
+		[Route("commands")]
+		public async Task<IActionResult> Command(string version, string device)
+		{
+			AirConditionerCommand command;
+			CancellationToken cancellationToken = new CancellationToken();
+			ActionResult result;
+			ContentResult contentResult;
+			string strCommandType;
+
+			Logging.WriteDebugLog("DeviceController.Command() Client Start: {0}:{1}", HttpContext.Connection.RemoteIpAddress.ToString(), HttpContext.Connection.RemotePort.ToString());
+
+			HttpContext.Response.Headers.Add("Access-Control-Allow-Headers", new Microsoft.Extensions.Primitives.StringValues("Accept, Content-Type, Authorization, Content-Length, X-Requested-With, X-Ninja-Token"));
+			HttpContext.Response.Headers.Add("Access-Control-Allow-Methods", new Microsoft.Extensions.Primitives.StringValues("GET,PUT,POST,DELETE,OPTIONS"));
+			HttpContext.Response.Headers.Add("Access-Control-Allow-Origin", new Microsoft.Extensions.Primitives.StringValues("*"));
+
+			if (!await AirConditioner.EventCommand.WaitOneAsync(_iTimeout, cancellationToken))
+				return new EmptyResult();
+
+			command = AirConditioner.GetCommand(out strCommandType);
+
+			if (strCommandType != "4" & strCommandType != "5")
+				result = new EmptyResult();
+			else
+			{
+				contentResult = new ContentResult();
+
+				contentResult.ContentType = "application/json";
+				contentResult.StatusCode = 200;
+
+				if (strCommandType == "4")
+				{
+					contentResult.Content = string.Format("{{\"DEVICE\":[{{\"G\":\"0\",\"V\":2,\"D\":4,\"DA\":{{\"amOn\":{0},\"tempTarget\":{1},\"fanSpeed\":{2},\"mode\":{3}}}}}]}}",
+						command.amOn ? "true" : "false",
+						command.tempTarget.ToString("F1"),
+						command.fanSpeed.ToString(),
+						command.mode.ToString()
+					);
+
+					Logging.WriteDebugLog("DeviceController.Command() Command: {0}", contentResult.Content);
+
+				}
+				else if (strCommandType == "5")
+				{
+					contentResult.Content = string.Format("{{\"DEVICE\":[{{\"G\":\"0\",\"V\":2,\"D\":5,\"DA\":{{\"enabledZones\":[{0}]}}}}]}}",
+						command.enabledZones
+					);
+
+					Logging.WriteDebugLog("DeviceController.Command() Command: {0}", contentResult.Content);
+				}
+
+				result = contentResult;
+			}
+
+			Logging.WriteDebugLog("DeviceController.Command() Client End: {0}:{1}", HttpContext.Connection.RemoteIpAddress.ToString(), HttpContext.Connection.RemotePort.ToString());
+
+			return result;
+		}
+
+		[Route("data")]
+		public IActionResult Data(string version, string device)
+		{
+			AirConditionerData data = new AirConditionerData();
+			AirConditionerDataHeader header;
+			AirConditionerDataHeader6 header6;
+			Dictionary<string, object> dDataField;
+			DataResponse response = new DataResponse();
+			StreamReader reader;
+			string strData;
+			Newtonsoft.Json.Linq.JArray aZones, aZoneTemperatures;
+
+			Logging.WriteDebugLog("DeviceController.Data() Client: {0}:{1}", HttpContext.Connection.RemoteIpAddress.ToString(), HttpContext.Connection.RemotePort.ToString());
+
+			HttpContext.Response.Headers.Add("Access-Control-Allow-Headers", new Microsoft.Extensions.Primitives.StringValues("Accept, Content-Type, Authorization, Content-Length, X-Requested-With, X-Ninja-Token"));
+			HttpContext.Response.Headers.Add("Access-Control-Allow-Methods", new Microsoft.Extensions.Primitives.StringValues("GET,PUT,POST,DELETE,OPTIONS"));
+			HttpContext.Response.Headers.Add("Access-Control-Allow-Origin", new Microsoft.Extensions.Primitives.StringValues("*"));
+
+			reader = new StreamReader(HttpContext.Request.Body);
+			strData = reader.ReadToEnd();
+			reader.Dispose();
+
+			try
+			{
+				header = JsonConvert.DeserializeObject<AirConditionerDataHeader>(strData);
+				switch (header.D)
+				{
+					case 6:
+						Logging.WriteDebugLog("DeviceController.Data() Data: {0}", strData);
+
+						header6 = JsonConvert.DeserializeObject<AirConditionerDataHeader6>(strData);
+
+						dDataField = header6.DA;
+
+						data.iCompressorActivity = int.Parse(dDataField["compressorActivity"].ToString());
+						data.strErrorCode = dDataField["errorCode"].ToString();
+						data.iFanContinuous = int.Parse(dDataField["fanIsCont"].ToString());
+						data.iFanSpeed = int.Parse(dDataField["fanSpeed"].ToString());
+						data.bOn = bool.Parse(dDataField["isOn"].ToString());
+						data.bESPOn = bool.Parse(dDataField["isInESP_Mode"].ToString());
+						data.iMode = int.Parse(dDataField["mode"].ToString());
+						data.dblRoomTemperature = double.Parse(dDataField["roomTemp_oC"].ToString());
+						data.dblSetTemperature = double.Parse(dDataField["setPoint"].ToString());
+
+						aZones = (Newtonsoft.Json.Linq.JArray) dDataField["enabledZones"];
+						data.bZone1 = (aZones[0].ToString() == "1");
+						data.bZone2 = (aZones[1].ToString() == "1");
+						data.bZone3 = (aZones[2].ToString() == "1");
+						data.bZone4 = (aZones[3].ToString() == "1");
+						data.bZone5 = (aZones[4].ToString() == "1");
+						data.bZone6 = (aZones[5].ToString() == "1");
+						data.bZone7 = (aZones[6].ToString() == "1");
+						data.bZone8 = (aZones[7].ToString() == "1");
+
+						aZoneTemperatures = (Newtonsoft.Json.Linq.JArray)dDataField["individualZoneTemperatures_oC"];
+						if (aZones[0].ToString() != "null") double.TryParse(aZoneTemperatures[0].ToString(), out data.dblZone1Temperature);
+						if (aZones[1].ToString() != "null") double.TryParse(aZoneTemperatures[1].ToString(), out data.dblZone2Temperature);
+						if (aZones[2].ToString() != "null") double.TryParse(aZoneTemperatures[2].ToString(), out data.dblZone3Temperature);
+						if (aZones[3].ToString() != "null") double.TryParse(aZoneTemperatures[3].ToString(), out data.dblZone4Temperature);
+						if (aZones[4].ToString() != "null") double.TryParse(aZoneTemperatures[4].ToString(), out data.dblZone5Temperature);
+						if (aZones[5].ToString() != "null") double.TryParse(aZoneTemperatures[5].ToString(), out data.dblZone6Temperature);
+						if (aZones[6].ToString() != "null") double.TryParse(aZoneTemperatures[6].ToString(), out data.dblZone7Temperature);
+						if (aZones[7].ToString() != "null") double.TryParse(aZoneTemperatures[7].ToString(), out data.dblZone8Temperature);							
+
+						AirConditioner.PostData(data);
+
+						break;
+				}
+			}
+			catch (Exception eException)
+			{
+				Logging.WriteDebugLogError("DeviceController.Data()", eException, "Unable to parse air conditioner data.");
+			}
+
+			response.result = 1;
+			response.error = null;
+			response.id = 0;
+
+			return new ObjectResult(response);
+		}		
+	}
+}
---
+++ hass-actronque/Service.cs
@@ -0,0 +1,197 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace HMX.HASSActron
+{
+    internal class Service
+    {
+		private static string _strServiceName = "hass-actron";
+		private static string _strDeviceName = "Air Conditioner";
+		private static string _strConfigFile = "/data/options.json";
+		private static bool _bRegisterZoneTemperatures = false;
+
+		public static bool RegisterZoneTemperatures
+		{
+			get { return _bRegisterZoneTemperatures; }
+		}
+
+		public static void Start()
+        {
+			IConfigurationRoot configuration;
+			IWebHost webHost;
+
+			Logging.WriteDebugLog("Service.Start()");
+
+			try
+			{
+				configuration = new ConfigurationBuilder().AddJsonFile(_strConfigFile, false, true).Build();
+			}
+			catch (Exception eException)
+			{
+				Logging.WriteDebugLogError("Service.Start()", eException, "Unable to build configuration instance.");
+				return;
+			}
+
+			bool.TryParse(configuration["RegisterZoneTemperatures"] ?? "false", out _bRegisterZoneTemperatures);
+
+			Logging.WriteDebugLog("Service.Start() RegisterZoneTemperatures: {0}", _bRegisterZoneTemperatures);
+		
+			MQTT.StartMQTT(configuration["MQTTBroker"] ?? "core-mosquitto", _strServiceName, configuration["MQTTUser"] ?? "", configuration["MQTTPassword"] ?? "", MQTTProcessor);
+
+			AirConditioner.Configure(configuration);
+
+			MQTTRegister();
+
+			try
+			{
+				webHost = new WebHostBuilder().UseKestrel().UseStartup<ASPNETCoreStartup>().UseConfiguration(configuration).UseUrls($"http://*:80/").Build();
+			}
+			catch (Exception eException)
+			{
+				Logging.WriteDebugLogError("Service.Start()", eException, "Unable to build Kestrel instance.");
+				return;
+			}
+
+			webHost.Run();
+		}
+
+		public static void Stop()
+		{
+			Logging.WriteDebugLog("Service.Stop()");
+
+			MQTT.StopMQTT();
+		}
+
+		private static void MQTTRegister()
+		{
+			Logging.WriteDebugLog("Service.MQTTRegister()");
+
+			MQTT.SendMessage("homeassistant/climate/actronaircon/config", "{{\"name\":\"{1}\",\"modes\":[\"off\",\"auto\",\"cool\",\"fan_only\",\"heat\"],\"fan_modes\":[\"high\",\"medium\",\"low\"],\"mode_command_topic\":\"actron/aircon/mode/set\",\"temperature_command_topic\":\"actron/aircon/temperature/set\",\"fan_mode_command_topic\":\"actron/aircon/fan/set\",\"min_temp\":\"12\",\"max_temp\":\"30\",\"temp_step\":\"0.5\",\"fan_mode_state_topic\":\"actron/aircon/fanmode\",\"action_topic\":\"actron/aircon/compressor\",\"temperature_state_topic\":\"actron/aircon/settemperature\",\"mode_state_topic\":\"actron/aircon/mode\",\"current_temperature_topic\":\"actron/aircon/temperature\",\"availability_topic\":\"{0}/status\"}}", _strServiceName.ToLower(), _strDeviceName);
+
+			foreach (int iZone in AirConditioner.Zones.Keys)
+			{
+				MQTT.SendMessage(string.Format("homeassistant/switch/actron/airconzone{0}/config", iZone), "{{\"name\":\"{0} Zone\",\"state_topic\":\"actron/aircon/zone{1}\",\"command_topic\":\"actron/aircon/zone{1}/set\",\"payload_on\":\"ON\",\"payload_off\":\"OFF\",\"state_on\":\"ON\",\"state_off\":\"OFF\",\"availability_topic\":\"{2}/status\"}}", AirConditioner.Zones[iZone].Name, iZone, _strServiceName.ToLower());
+				MQTT.Subscribe("actron/aircon/zone{0}/set", iZone);
+
+				if (_bRegisterZoneTemperatures)
+					MQTT.SendMessage(string.Format("homeassistant/sensor/actron/airconzone{0}/config", iZone), "{{\"name\":\"{0}\",\"state_topic\":\"actron/aircon/zone{1}/temperature\",\"unit_of_measurement\":\"\u00B0C\",\"availability_topic\":\"{2}/status\"}}", AirConditioner.Zones[iZone].Name, iZone, _strServiceName.ToLower());
+				else
+					MQTT.SendMessage(string.Format("homeassistant/sensor/actron/airconzone{0}/config", iZone), "{{}}"); // Clear existing devices
+			}
+			
+			MQTT.Subscribe("actron/aircon/mode/set");
+			MQTT.Subscribe("actron/aircon/fan/set");
+			MQTT.Subscribe("actron/aircon/temperature/set");
+		}
+
+		private static void MQTTProcessor(string strTopic, string strPayload)
+		{
+			long lRequestId = 0;
+			double dblTemperature = 0;
+
+			Logging.WriteDebugLog("Service.MQTTProcessor() {0}", strTopic);
+
+			switch (strTopic)
+			{
+				case "actron/aircon/zone1/set":
+					AirConditioner.ChangeZone(lRequestId, 1, strPayload == "ON" ? true : false);
+					break;
+
+				case "actron/aircon/zone2/set":
+					AirConditioner.ChangeZone(lRequestId, 2, strPayload == "ON" ? true : false);
+					break;
+
+				case "actron/aircon/zone3/set":
+					AirConditioner.ChangeZone(lRequestId, 3, strPayload == "ON" ? true : false);
+					break;
+
+				case "actron/aircon/zone4/set":
+					AirConditioner.ChangeZone(lRequestId, 4, strPayload == "ON" ? true : false);
+					break;
+
+				case "actron/aircon/zone5/set":
+					AirConditioner.ChangeZone(lRequestId, 5, strPayload == "ON" ? true : false);
+					break;
+
+				case "actron/aircon/zone6/set":
+					AirConditioner.ChangeZone(lRequestId, 6, strPayload == "ON" ? true : false);
+					break;
+
+				case "actron/aircon/zone7/set":
+					AirConditioner.ChangeZone(lRequestId, 7, strPayload == "ON" ? true : false);
+					break;
+
+				case "actron/aircon/zone8/set":
+					AirConditioner.ChangeZone(lRequestId, 8, strPayload == "ON" ? true : false);
+					break;
+
+				case "actron/aircon/mode/set":
+					Logging.WriteDebugLog("ServiceCore.MQTTProcessor() {0}: {1}", strTopic, strPayload);
+
+					switch (strPayload)
+					{
+						case "off":
+							AirConditioner.ChangeMode(lRequestId, AirConditionerMode.None);
+
+							break;
+
+						case "auto":
+							AirConditioner.ChangeMode(lRequestId, AirConditionerMode.Automatic);
+
+							break;
+
+						case "cool":
+							AirConditioner.ChangeMode(lRequestId, AirConditionerMode.Cooling);
+
+							break;
+
+						case "heat":
+							AirConditioner.ChangeMode(lRequestId, AirConditionerMode.Heating);
+
+							break;
+
+						case "fan_only":
+							AirConditioner.ChangeMode(lRequestId, AirConditionerMode.FanOnly);
+
+							break;
+					}
+
+					break;
+
+				case "actron/aircon/fan/set":
+					Logging.WriteDebugLog("Service.MQTTProcessor() {0}: {1}", strTopic, strPayload);
+
+					switch (strPayload)
+					{
+						case "low":
+							AirConditioner.ChangeFanSpeed(lRequestId, FanSpeed.Low);
+
+							break;
+
+						case "medium":
+							AirConditioner.ChangeFanSpeed(lRequestId, FanSpeed.Medium);
+
+							break;
+
+						case "high":
+							AirConditioner.ChangeFanSpeed(lRequestId, FanSpeed.High);
+
+							break;
+					}
+
+					break;
+
+				case "actron/aircon/temperature/set":
+					Logging.WriteDebugLog("Service.MQTTProcessor() {0}: {1}", strTopic, strPayload);
+
+					if (double.TryParse(strPayload, out dblTemperature))
+						AirConditioner.ChangeTemperature(lRequestId, dblTemperature);
+
+					break;
+			}
+		}
+    }
+
+}
