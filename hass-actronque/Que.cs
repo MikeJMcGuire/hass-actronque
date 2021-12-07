6d02d0e592a6eead485acaa3bba667ff7206e7a1
MikeJMcGuire <mike@mcguire.mx>
2021-12-07 15:53:17 +1100

    Updates

--- hass-actronque/Que.cs
+++ hass-actronque/Que.cs
@@ -632,6 +632,11 @@ namespace HMX.HASSActronQue
 
 					Logging.WriteDebugLog("Que.GetAirConditionerFullStatus() [0x{0}] Responded (Encoding {1}, {2} bytes)", lRequestId.ToString("X8"), httpResponse.Content.Headers.ContentEncoding.ToString() == "" ? "N/A" : httpResponse.Content.Headers.ContentEncoding.ToString(), (httpResponse.Content.Headers.ContentLength ?? 0) == 0 ? "N/A" : httpResponse.Content.Headers.ContentLength.ToString());
 
+					lock (_oLockData)
+					{
+						_airConditionerData.LastUpdated = DateTime.Now;
+					}
+
 					jsonResponse = JsonConvert.DeserializeObject(strResponse);
 
 					// Compressor Mode
