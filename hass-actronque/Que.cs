3c2bc687cd8cfc556604ec6f19d473c7dea403fb
MikeJMcGuire <mike@mcguire.mx>
2022-01-17 19:26:16 +1100

    v2022.1.0 Initial

--- hass-actronque/Que.cs
+++ hass-actronque/Que.cs
@@ -1210,15 +1210,18 @@ namespace HMX.HASSActronQue
 								}
 
 								// Compressor Power
-								if (!double.TryParse(jsonResponse.events[iEvent].data.LiveAircon.OutdoorUnit.CompPower.ToString(), out dblTemp))
-									Logging.WriteDebugLog("Que.GetAirConditionerEvents() [0x{0}] Unable to read state information: {1}", lRequestId.ToString("X8"), "LiveAircon.OutdoorUnit.CompPower");
-								else
+								if (jsonResponse.events[iEvent].data.LiveAircon.ContainsKey("OutdoorUnit"))
 								{
-									lock (_oLockData)
+									if (!double.TryParse(jsonResponse.events[iEvent].data.LiveAircon.OutdoorUnit.CompPower.ToString(), out dblTemp))
+										Logging.WriteDebugLog("Que.GetAirConditionerEvents() [0x{0}] Unable to read state information: {1}", lRequestId.ToString("X8"), "LiveAircon.OutdoorUnit.CompPower");
+									else
 									{
-										_airConditionerData.CompressorPower = dblTemp;
+										lock (_oLockData)
+										{
+											_airConditionerData.CompressorPower = dblTemp;
+										}
 									}
-								}								
+								}
 
 								// On
 								if (!bool.TryParse(jsonResponse.events[iEvent].data.UserAirconSettings.isOn.ToString(), out bTemp))
