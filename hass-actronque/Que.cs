e6862da23c7d15835ed138496c827179f762def4
MikeJMcGuire <mike@mcguire.mx>
2022-10-17 19:41:23 +1100

    Updates

--- hass-actronque/Que.cs
+++ hass-actronque/Que.cs
@@ -494,6 +494,9 @@ namespace HMX.HASSActronQue
 
 					Logging.WriteDebugLog("Que.GetAirConditionerSerial() [0x{0}] Responded (Encoding {1}, {2} bytes)", lRequestId.ToString("X8"), httpResponse.Content.Headers.ContentEncoding.ToString() == "" ? "N/A" : httpResponse.Content.Headers.ContentEncoding.ToString(), (httpResponse.Content.Headers.ContentLength ?? 0) == 0 ? "N/A" : httpResponse.Content.Headers.ContentLength.ToString());
 
+					if (Service.IsDevelopment)
+						Logging.WriteDebugLog("Que.GetAirConditionerSerial() [0x{0}] Response: {1}", lRequestId.ToString("X8"), strResponse);
+
 					strResponse = strResponse.Replace("ac-system", "acsystem");
 
 					jsonResponse = JsonConvert.DeserializeObject(strResponse);
